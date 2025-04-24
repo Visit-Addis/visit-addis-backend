d1f190d8940a51850e5a89a20eb58d917d183464
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-25 03:10:18 +0600

    logger implemented

---
+++ src/configs/wins.logger.js
@@ -0,0 +1,41 @@
+import { format, transports, createLogger, addColors } from "winston";
+const { combine, timestamp, json, simple, colorize } = format;
+
+addColors({
+  error: "red bold",
+  warn: "yellow bold",
+  info: "cyan bold",
+  http: "magenta bold",
+  debug: "blue bold",
+});
+
+// console transport
+const consoleTransport = new transports.Console({
+  format: combine(colorize({ all: true }), simple()),
+});
+//create http logger
+const httpLogger = createLogger({
+  level: "http",
+  format: json(),
+  transports: [consoleTransport],
+});
+//create error logger
+const errorLogger = createLogger({
+  level: "error",
+  format: combine(json(), timestamp()),
+  transports: [consoleTransport],
+});
+//create email sent logger
+const emailLogger = createLogger({
+  level: "info",
+  format: combine(json(), timestamp()),
+  transports: [consoleTransport],
+});
+
+const infoLogger = createLogger({
+  level: "info",
+  format: combine(colorize({ all: true })),
+  transports: [consoleTransport],
+});
+
+export default { httpLogger, errorLogger, emailLogger, infoLogger };

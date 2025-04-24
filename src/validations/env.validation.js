a804819acd4c9fffcd8018de7f7a70dcabf58917
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-25 03:11:51 +0600

    env variables added and  implemented

---
+++ src/validations/env.validation.js
@@ -0,0 +1,17 @@
+import Joi from "joi";
+const envSchema = Joi.object()
+  .keys({
+    PORT: Joi.number().default(3000).description("port number"),
+    DB_URL: Joi.string()
+      .default("mongodb://localhost:27017/visit-addis")
+      .description("mongodb url"),
+    NODE_ENV: Joi.string().default("production").required(),
+    ACESS_TOKEN_EXPIRES_IN_MINUTES: Joi.number().required(),
+    REFRESH_TOKEN_EXPIRES_IN_DAYS: Joi.number().required(),
+    RESET_PASSWORD_TOKEN_EXPIRES_IN_SECONDS: Joi.number().required(),
+    EMAIL_VERIFICATION_TOKEN_EXPIRES_IN_SECONDS: Joi.number().required(),
+    JWT_SECRET_KEY: Joi.string().required(),
+    SERVER_URL: Joi.string().required(),
+  })
+  .unknown();
+export default envSchema;

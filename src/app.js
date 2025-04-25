fea7c38e064526bf331a0d55e431d35a2d6ba7ef
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-26 02:59:26 +0600

    rate limiter  integrated

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { errorHandler } from "./middlewares/index.js";
+import { errorHandler, authLimiter } from "./middlewares/index.js";
 import { CustomError } from "./utils/index.js";
+import apiRoute from "./routes/v1/index.js";
+import { envVar } from "./configs/env.vars.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if (envVar.env === "production") {
+  app.use("api/v1/auth", authLimiter);
+}
+app.use("/api/v1", apiRoute);
 
 app.all("/*unknown", (req, res, next) => {
   const message = `${req.originalUrl} not found`;

a7a61777105bd23d3e882fdef82b89c2702d218f
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-25 17:51:52 +0600

    global error hhandler integrated

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import express from "express";
+import { errorHandler } from "./middlewares/index.js";
+import { CustomError } from "./utils/index.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.all("/*unknown", (req, res, next) => {
+  const message = `${req.originalUrl} not found`;
+  const statusCode = 404;
+  const error = new CustomError(statusCode, message, true);
+  next(error);
+});
+
+app.use(errorHandler.convertError);
+app.use(errorHandler.handleGlobalError);
 
 export default app;

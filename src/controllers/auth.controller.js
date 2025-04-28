ae354c8ae7b757ee1485eee0dc983a516a892cb8
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-29 01:10:46 +0600

    google auth logic implemented

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,10 @@
+import { DateTime } from "luxon";
 import { handleCatchError } from "../utils/index.js";
 import { authService } from "../services/index.js";
+import { CustomError } from "../utils/index.js";
+import { envVar } from "../configs/env.vars.js";
+import { tokenTypes } from "../configs/constants.js";
+import passport from "../configs/passport.js";
 
 const register = handleCatchError(async (req, res) => {
   const { email, password } = req.body;
@@ -31,10 +36,37 @@ const resetPassword = handleCatchError(async (req, res) => {
   res.status(200).send(message);
 });
 
+const handleGoogleAuthSuccess = (req, res) => {
+  const token = tokenService.generateToken(
+    req.user.id,
+    req.user.role,
+    tokenTypes.ACCESS,
+    DateTime.now().plus({ minutes: envVar.token.acessTokenExp })
+  );
+  res.status(200).send(token);
+};
+
+const handleGoogleAuthError = (req, res, next) => {
+  return next(new CustomError(400, "Google authentication failed", true));
+};
+
+const initiateGoogleAuth = passport.authenticate("google", {
+  scope: ["profile", "email"],
+});
+
+const handleGoogleAuthCallback = passport.authenticate("google", {
+  failureRedirect: `${envVar.serverUrl}/api/v1/auth/login`,
+  session: true,
+});
+
 export default {
   register,
   login,
   acceptPasswordResetRequest,
   sentResetPasswordForm,
   resetPassword,
+  initiateGoogleAuth,
+  handleGoogleAuthCallback,
+  handleGoogleAuthSuccess,
+  handleGoogleAuthError,
 };

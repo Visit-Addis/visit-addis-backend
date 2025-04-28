e35f8dbdc0e58d6b489ec32c7d6e1e70ab8965f9
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-04-29 01:45:33 +0600

    authRoute implemented

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -1,4 +1,17 @@
 import express from "express";
-
+import { authController } from "../../controllers/index.js";
 const Router = express.Router();
+Router.route("/register").post(authController.register);
+Router.route("/login").post(authController.login);
+Router.route("/password-reset-request").post(
+  authController.acceptPasswordResetRequest
+);
+Router.route("/password-reset-form").get(authController.sentResetPasswordForm);
+Router.route("/reset-password").post(authController.resetPassword);
+Router.route("/google").post(authController.initiateGoogleAuth);
+Router.route("/google/callback").get(
+  authController.handleGoogleAuthCallback,
+  authController.handleGoogleAuthSuccess,
+  authController.handleGoogleAuthError
+);
 export default Router;

2196daf7c408e33fffe9ff96569f9c035881731f
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-05-01 13:08:53 +0600

    add feature

--- src/services/attraction.service.js
+++ src/services/attraction.service.js
@@ -1,6 +1,36 @@
 import { Attraction } from "../models/index.js";
 import { CustomError } from "../utils/index.js";
 
+const postAttraction = async (data) => {
+  const attraction = await Attraction.create(data);
+  if (!attraction) {
+    throw new CustomError(400, "attraction post failed", true);
+  }
+  return attraction;
+};
+
+const deleteAttraction = async (id) => {
+  const deletedAttraction = await Attraction.findByIdAndDelete(id);
+  if (!deleteAttraction) {
+    throw new CustomError(400, "Attraction deletion failed", true);
+  }
+  return "attraction deleted successfully";
+};
+const updateAttraction = async (id, data) => {
+  const attraction = await Attraction.findById(id);
+  if (!attraction) {
+    throw new CustomError(400, "NO attraction found with this id", true);
+  }
+  Object.keys(data).forEach((key) => {
+    attraction[key] = data[key];
+  });
+  const savedAttraction = await attraction.save();
+  if (!savedAttraction) {
+    throw new CustomError(400, "unable to update attraction", true);
+  }
+  return "attraction update successfully";
+};
+
 const getAttractions = async () => {
   const attractions = await Attraction.find(
     {},
@@ -40,4 +70,11 @@ const searchAttraction = async (searchItemes) => {
   return attractions;
 };
 
-export default { getAttractions, getAttractionDetails, searchAttraction };
+export default {
+  getAttractions,
+  getAttractionDetails,
+  searchAttraction,
+  postAttraction,
+  deleteAttraction,
+  updateAttraction,
+};

ef0bf0dabbbacd903d8106fb3ab9698210fbdbe9
Hiluf Tesfay <hiluftesfay43@gmail.com>
2025-05-01 13:25:20 +0600

    add feature

--- src/routes/v1/attraction.route.js
+++ src/routes/v1/attraction.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { attractionController } from "../../controllers/index.js";
 const Router = express.Router();
-
-Router.route("/").get(attractionController.getAttractions);
-Router.route("/:id").get(attractionController.getAttractionDetail);
+Router.route("/")
+  .get(attractionController.getAttractions)
+  .post(attractionController.postAttraction);
+Router.route("/:id")
+  .get(attractionController.getAttractionDetail)
+  .put(attractionController.updateAttraction)
+  .delete(attractionController.deleteAttraction);
 Router.route("/search").get(attractionController.searchAttractions);
 
 export default Router;
